5798b08051362e6c1791b41dba6284aa0c6ee261
harshatProject <harsha.t@hotmail.com>
2021-10-15 08:52:20 +0530

    ngIf directives

--- Angular2000/src/app/app.module.ts
+++ Angular2000/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DirectiveExampleComponent } from './directive-example/directive-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectiveExampleComponent
   ],
   imports: [
     BrowserModule
