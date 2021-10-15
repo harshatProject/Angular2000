b6df324d3a8c6836ad3a2d4488f413c662a06a4c
harshatProject <harsha.t@hotmail.com>
2021-10-15 11:17:22 +0530

    Angular:5 ngFor directives

--- Angular2000/src/app/directive-example/directive-example.component.ts
+++ Angular2000/src/app/directive-example/directive-example.component.ts
@@ -3,22 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directive-example',
   template: `
-  <div>
-    <p> Simple ngIf </p>
-    <p *ngIf="isDataAvailable">Data Avilable</p>
-  </div>
-
-  <div >
-  <p> ngIf with else, using ng-template </p>
-    <div  [ngStyle] = "{backgroundColor:getColor()}"
-          [ngClass] = "{whiteText: !isSunday()}"
-    > 
-      <p *ngIf="isSunday(); else nonSunday" >Today is Sunday</p>
-      <ng-template #nonSunday>
-      <p  >Today is a weekday and is non sunday</p>   
-    </ng-template>
-    </div> 
-  </div>
+  
+    <div *ngFor="let day of daysOfWeek">
+      <p>{{day}}</p>
+    </div>
+  
   `,
   styles:[`
   .whiteText{
@@ -31,20 +20,10 @@ export class DirectiveExampleComponent implements OnInit {
 
   isDataAvailable:boolean = false;
 
-  constructor() {
-    setTimeout(()=>{this.isDataAvailable =true}, 3000);
-   }
+  daysOfWeek:any =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
 
-   isSunday():boolean {
-     var today = new Date();
-     console.log(today.getDay());
-    //return 10%2 ===0 ?true:false;
-    return today.getDay()===6 ?  true:false;
-   } 
-
-   getColor(){
-    return this.isSunday()===true ? 'red':'green';
-   }
+  constructor() {    
+   }  
 
   ngOnInit(): void {
   }
