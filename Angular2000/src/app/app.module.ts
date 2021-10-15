b532c5788a00b788601f52dc6f9215b7d0176a51
harshatProject <harsha.t@hotmail.com>
2021-10-15 13:56:27 +0530

    Angular:6 Passing info from Parent to Child

--- Angular2000/src/app/app.module.ts
+++ Angular2000/src/app/app.module.ts
@@ -3,11 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DirectiveExampleComponent } from './directive-example/directive-example.component';
+import { TodoItemComponent, TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveExampleComponent
+    DirectiveExampleComponent,
+    TodoListComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
--- Angular2000/src/app/directive-example/directive-example.component.ts
+++ Angular2000/src/app/directive-example/directive-example.component.ts
@@ -26,6 +26,7 @@ export class DirectiveExampleComponent implements OnInit {
    }  
 
   ngOnInit(): void {
+    
   }
 
 }
