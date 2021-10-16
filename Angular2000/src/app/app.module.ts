8eb7043ef600d47775fcadb6fad3f14b2b845a11
harshatProject <harsha.t@hotmail.com>
2021-10-16 20:53:41 +0530

    Angular:7 ngSwitch directive

--- Angular2000/src/app/app.module.ts
+++ Angular2000/src/app/app.module.ts
@@ -3,14 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DirectiveExampleComponent } from './directive-example/directive-example.component';
-import { TodoItemComponent, TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveExampleComponent,
-    TodoListComponent,
-    TodoItemComponent
+    DirectiveExampleComponent    
   ],
   imports: [
     BrowserModule
--- Angular2000/src/app/directive-example/directive-example.component.ts
+++ Angular2000/src/app/directive-example/directive-example.component.ts
@@ -3,30 +3,45 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-directive-example',
   template: `
-  
-    <div *ngFor="let day of daysOfWeek">
-      <p>{{day}}</p>
-    </div>
-  
+    <div [ngSwitch]=numDay>
+      <div *ngSwitchCase="1"><p>Today is {{dayOftheWeek}}</p></div>
+      <div *ngSwitchCase="2"><p>Today is {{dayOftheWeek}}</p></div>
+      <div *ngSwitchCase="3"><p>Today is {{dayOftheWeek}}</p></div>
+      <div *ngSwitchCase="4"><p>Today is {{dayOftheWeek}}</p></div>
+      <div *ngSwitchCase="5"><p>Today is {{dayOftheWeek}}</p></div>
+      <div *ngSwitchCase="6"><p>Today is {{dayOftheWeek}}</p></div>
+      <div *ngSwitchCase="7"><p>Today is {{dayOftheWeek}}</p></div>
+      <div *ngSwitchDefault>This is Default</div>
+    </div>  
   `,
   styles:[`
   .whiteText{
     color:white;
   }
-
   `]
 })
 export class DirectiveExampleComponent implements OnInit {
 
   isDataAvailable:boolean = false;
+  dayOftheWeek: any;
 
-  daysOfWeek:any =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+  daysOfWeek:any =['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
 
-  constructor() {    
-   }  
+  
+  num: number= 0;
+  numDay: number | undefined;
 
-  ngOnInit(): void {
-    
+  constructor() {
+    this.whatDayToday()
   }
 
+   whatDayToday(){
+    var d = new Date();
+    this.numDay  = d.getDay();
+    this.dayOftheWeek = this.daysOfWeek[this.numDay];
+    console.log(this.numDay);
+   }
+
+  ngOnInit(): void {
+  }
 }
